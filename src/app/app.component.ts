84166edeaef346ca74323b7b7392703481490265
Vinoth Kumar <vinoth.kumar.esthaki@gmail.com>
2021-09-17 18:34:26 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qr-code';
+  public qrForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.qrForm = this.fb.group({
+      value: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      size: ['', Validators.compose([Validators.required, Validators.max(600)])]
+    });
+  }
+
 }
